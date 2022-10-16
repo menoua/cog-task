ac13291e3c678223672da1bb7ffca8d58c1e592e
Menoua Keshishian <39179450+menoua@users.noreply.github.com>
2022-10-16 16:34:43 -0400

    Fix thread panic message

--- src/signal.rs
+++ src/signal.rs
@@ -1,62 +1,36 @@
 use std::collections::VecDeque;
+use std::fmt::Debug;
 use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{mpsc, Arc, Mutex};
 
-#[derive(Debug)]
-pub struct Queue<T>(Arc<Mutex<VecDeque<T>>>);
+pub const QUEUE_SIZE: usize = 64;
+pub type Queue<T> = Arc<Mutex<VecDeque<T>>>;
 
 #[derive(Debug)]
-pub struct QReader<T>(Queue<T>, Sender<()>, Receiver<()>);
+pub struct QReader<T: Debug>(Queue<T>, Sender<()>, Receiver<()>);
 
 #[derive(Debug, Clone)]
-pub struct QWriter<T>(Queue<T>, Sender<()>);
+pub struct QWriter<T: Debug>(Queue<T>, Sender<()>);
 
-impl<T> Queue<T> {
-    fn new() -> Self {
-        Self(Arc::new(Mutex::new(VecDeque::with_capacity(64))))
-    }
-
-    #[inline(always)]
-    fn push(&mut self, msg: impl Into<T>) {
-        self.0.lock().unwrap().push_back(msg.into());
-    }
-
-    #[inline(always)]
-    fn pop(&mut self) -> Option<T> {
-        self.0.lock().unwrap().pop_front()
-    }
-
-    #[inline(always)]
-    pub fn clear(&mut self) {
-        self.0.lock().unwrap().clear();
-    }
-}
-
-impl<T> Clone for Queue<T> {
-    fn clone(&self) -> Self {
-        Queue(self.0.clone())
-    }
-}
-
-impl<T> QReader<T> {
+impl<T: Debug> QReader<T> {
     pub fn new() -> Self {
-        let queue = Queue::new();
+        let queue = Arc::new(Mutex::new(VecDeque::with_capacity(QUEUE_SIZE)));
         let (tx, rx) = mpsc::channel();
         Self(queue, tx, rx)
     }
 
     #[inline(always)]
     pub fn push(&mut self, msg: impl Into<T>) {
-        self.0.push(msg);
-        self.1
-            .send(())
-            .expect("Signal buffer was dropped before the signal handle.");
+        let mut queue = self.0.lock().unwrap();
+        if self.1.send(()).is_ok() {
+            queue.push_back(msg.into());
+        }
     }
 
     #[inline(always)]
     pub fn pop(&mut self) -> Option<T> {
         if self.2.recv().is_ok() {
-            Some(self.0.pop().unwrap())
+            Some(self.0.lock().unwrap().pop_front().unwrap())
         } else {
             None
         }
@@ -65,7 +39,7 @@ impl<T> QReader<T> {
     #[inline(always)]
     pub fn try_pop(&mut self) -> Option<T> {
         if let Ok(()) = self.2.try_recv() {
-            Some(self.0.pop().unwrap())
+            Some(self.0.lock().unwrap().pop_front().unwrap())
         } else {
             None
         }
@@ -73,7 +47,7 @@ impl<T> QReader<T> {
 
     #[inline(always)]
     pub fn clear(&mut self) {
-        self.0.clear();
+        self.0.lock().unwrap().clear();
     }
 
     #[inline(always)]
@@ -82,12 +56,12 @@ impl<T> QReader<T> {
     }
 }
 
-impl<T> QWriter<T> {
+impl<T: Debug> QWriter<T> {
     #[inline(always)]
     pub fn push(&mut self, msg: impl Into<T>) {
-        self.0.push(msg);
-        self.1
-            .send(())
-            .expect("Signal buffer was dropped before the signal handle.");
+        let mut queue = self.0.lock().unwrap();
+        if self.1.send(()).is_ok() {
+            queue.push_back(msg.into());
+        }
     }
 }
